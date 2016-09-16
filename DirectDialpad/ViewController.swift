8bdb409c148d8bcf4d0b65c693e21ad0eea9f9dc
David Oliver Barreto Rodríguez <oliver.barreto.online@gmail.com>
2016-09-16 13:32:31 +0100

    Initial Commit

---
+++ DirectDialpad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DirectDialpad
+//
+//  Created by David Oliver Barreto Rodríguez on 16/9/16.
+//  Copyright © 2016 Oliver Barreto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
